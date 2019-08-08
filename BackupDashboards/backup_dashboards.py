3ec869e0e589f267d5e96c487fd79fedafca268d
Nathan Giusti <bigbadbyte2@gmail.com>
2019-08-08 11:21:32 -0400

    Updated with new cropping and resizing

--- BackupDashboards/backup_dashboards.py
+++ BackupDashboards/backup_dashboards.py
@@ -66,16 +66,20 @@ def parse_error_response(response, error_text, exit_on_error=False):
     return True
 
 
-def build_path(folder, dashboard_id, file_format):
+def build_path(folder, dashboard_id, file_format, file_num=None):
     """
     Builds the path to save the export to
 
     :param folder: Folder to save dashboard to
     :param dashboard_id: Id of dashboard
     :param file_format: Format to save dashboard to
+    :param file_num: A number to place after the file name in case multiple images from the same dash are taken
     :return: A string in the format <path>/dashboard_id.file_format
     """
-    return "{}\\{}.{}".format(folder, dashboard_id, file_format)
+    if file_num:
+        return "{}\\{}-{}.{}".format(folder, dashboard_id, file_num, file_format)
+    else:
+        return "{}\\{}.{}".format(folder, dashboard_id, file_format)
 
 
 def call_export_api(headers, request_string):
@@ -112,6 +116,32 @@ def export_dash(host, dashboard, headers, file_folder):
             out_file.write(resp.content)
 
 
+def generate_file(host, format_vars, dashboard, headers, file_folder):
+    query_string = ''
+    if 'query_params' in format_vars:
+        query_string = build_query_string(format_vars['query_params'])
+    request_string = '{}/api/v1/dashboards/{}/export/{}?{}'.format(host, dashboard, format_vars['file_type'], query_string)
+    resp = call_export_api(headers, request_string)
+    file_path = build_path(file_folder, dashboard, format_vars['file_type'])
+    if resp:
+        with open(file_path, 'wb') as out_file:
+            for chunk in resp:
+                out_file.write(chunk)
+        print("Dashboard {} exported to {} successfully".format(dashboard, format_vars['file_type']))
+    return file_path
+
+
+def create_cropping_list(cropping_string):
+    coord_list = []
+    for coord_str in cropping_string:
+        if len(coord_str.split(',')) != 4:
+            print("Invalid coordinate string {}. Requires 4 integers.".format(cropping_string))
+            continue
+        for coord in coord_str.split(','):
+            coord_list.append(int(coord))
+    return coord_list
+
+
 def export_png(host, format_vars, dashboard, headers, file_folder, cropping):
     """
     Exports dashboard to png
@@ -121,25 +151,30 @@ def export_png(host, format_vars, dashboard, headers, file_folder, cropping):
     :param dashboard: The dashboard id
     :param headers: The header of the response file with authorization code
     :param file_folder: Folder to export dashboard to
+    :param cropping: The cropping yaml section
     :return: Nothing
     """
-    query_string = ''
-    if 'query_params' in format_vars:
-        query_string = build_query_string(format_vars['query_params'])
-    request_string = '{}/api/v1/dashboards/{}/export/png?{}'.format(host, dashboard, query_string)
-    resp = call_export_api(headers, request_string)
-    file_path = build_path(file_folder, dashboard, format_vars['file_type'])
-    if resp:
-        with open(file_path, 'wb') as out_file:
-            for chunk in resp:
-                out_file.write(chunk)
-        print("Dashboard {} exported to {} successfully".format(dashboard, format_vars['file_type']))
-        
-    if cropping and dashboard in cropping:
-        image_obj = Image.open(file_path)
-        cropped_image = image_obj.crop((cropping[dashboard]['x1'], cropping[dashboard]['y1'],
-                                        cropping[dashboard]['x2'], cropping[dashboard]['y2']))
-        cropped_image.save(file_path)
+    if dashboard in cropping:
+        coord_list = create_cropping_list(cropping[dashboard])
+        i = 0
+        while i < len(coord_list):
+            png_file = generate_file(host, format_vars, dashboard, headers, file_folder)
+            width = format_vars['query_params']['width'] if 'width' in format_vars['query_params'] else 1300
+            image_obj = Image.open(png_file)
+            x1 = coord_list[i]
+            y1 = coord_list[i + 1]
+            x2 = coord_list[i + 2]
+            y2 = coord_list[i + 3]
+            i += 4
+            cropped_image = image_obj.crop((x1, y1, x2, y2))
+            scaling_ratio = width / (x2 - x1)
+            y_coord = scaling_ratio * (y2 - y1)
+            resized_image = cropped_image.resize((width, int(y_coord)))
+            file_path = build_path(file_folder, dashboard, format_vars['file_type'], int(i / 4))
+            print('Cropped image to {} by {}'.format(width, int(y_coord)))
+            resized_image.save(file_path)
+    else:
+        return generate_file(host, format_vars, dashboard, headers, file_folder)
 
 
 def export_pdf(host, format_vars, dashboard, headers, file_folder):
@@ -158,19 +193,14 @@ def export_pdf(host, format_vars, dashboard, headers, file_folder):
         if param not in format_vars['query_params']:
             print('Missing parameter {}. Cannot export dashboard {}'.format())
             return
-    query_string = build_query_string(format_vars['query_params'])
-    request_string = '{}/api/v1/dashboards/{}/export/pdf?{}'.format(host, dashboard, query_string)
-    resp = call_export_api(headers, request_string)
-    if resp:
-        print("Dashboard {} exported to {} successfully".format(dashboard, format_vars['file_type']))
-        with open(build_path(file_folder, dashboard, format_vars['file_type']), 'wb') as out_file:
-            out_file.write(resp.content)
+    generate_file(host, format_vars, dashboard, headers, file_folder)
 
 
 def get_dashboards(host, headers, query_parameters):
     """
     Calls for list of dashboards from API
 
+    :param host: The host address
     :param headers: The header of the response file with authorization code
     :param query_parameters: Query params to pass to api call
     :return: A list of dashboards ids returned by the API
@@ -228,7 +258,7 @@ def main():
         elif format_vars['file_type'] == 'pdf':
             export_pdf(host, format_vars, dashboard, headers, file_folder)
         elif format_vars['file_type'] == 'dash':
-            export_dash(host, dashboard, headers, file_folder)
+            generate_file(host, format_vars, dashboard, headers, file_folder)
 
     print('Backups complete')
 
