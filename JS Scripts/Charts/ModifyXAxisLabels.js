197989dea7a28c1b966bcfae922330054a16ae76
Nathan Giusti <bigbadbyte2@gmail.com>
2019-08-08 09:48:38 -0400

    Cleaned up x axis script

--- JS Scripts/Charts/ModifyXAxisLabels.js
+++ JS Scripts/Charts/ModifyXAxisLabels.js
@@ -1,12 +1,12 @@
 
 widget.on('render', function(widget,args){
-	var cats = widget.queryResult.xAxis.categories //Need to save and reassign the categories
 	widget.queryResult.xAxis = {
-	    categories: cats,
+	    categories: widget.queryResult.xAxis.categories,
 	    //Runs every value in the category through this function 
 	    labels: {
 	        formatter: function () {
-	           return '<a target="_blank" href="https://en.wikipedia.org/wiki/'+ this.value + '">' + this.value + '</a>';
+	        	var res = this.value.split("/");
+	           	return '<a title="' + res[1] + '" target="_blank" href="https://en.wikipedia.org/wiki/'+ res[0] + '">' + res[0] + '</a>';
 	        },
 	        useHTML: true // Needed to ensure full html is processed
 	    }
